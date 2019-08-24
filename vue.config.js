cea54b961d34eb8883eef96f56530d33bd51c7cd
marti <martinmail@mail.ru>
2019-08-25 01:32:09 +0300

    dep

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    transpileDependencies: [
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/stat_kek/'
+    : '/',
+  transpileDependencies: [
       'vue-echarts',
       'resize-detector'
     ]
