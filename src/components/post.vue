a5f9bb620cd1576548c29bd55ccf2ed7fd465530
martins0n <martinmail@mail.ru>
2019-10-14 09:00:36 +0300

    post external link

--- src/components/post.vue
+++ src/components/post.vue
@@ -2,7 +2,8 @@
     <div>
         <h2> {{post.title}} </h2>
         <p> {{post.body}} </p>
-        <a > {{ post.url }} </a>
+        <a :href="'//' + post.url"> {{ post.url }} </a>
+        <hr align="center" width="500"  />
     </div>
 </template>
 
